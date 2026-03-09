46858b256caa52da0d5db3b49850821037a25822
Stiviiiu <osemenova2025@gmail.com>
2026-03-09 20:09:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-TOKEN = os.environ.get("BOT_TOKEN", "8608911191:AAFHxjt4Lo5YFAJ8WhTWifCnPmiglqmVC2w")
+TOKEN = os.environ.get("BOT_TOKEN", "8608911191:AAHIK7VIskJJqrwt8lMZQ5ZDJwdUvdwRF-0")
 ADMIN_IDS = [int(x) for x in os.environ.get("ADMIN_IDS", "6458164021").split(",")]
 
 # Подключение к PostgreSQL (Supabase)
