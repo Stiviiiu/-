b2a21aa6a0c61ea6c42e0082ffafd8d213d3461c
Stiviiiu <osemenova2025@gmail.com>
2026-03-09 22:03:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 TOKEN = os.environ.get("BOT_TOKEN", "8608911191:AAHRgzz6SJRZ8YLYgn_LBmed8aGwcb9hc5A")
-ADMIN_IDS = [int(x) for x in os.environ.get("ADMIN_IDS", "6458164021").split(",")]
+ADMIN_IDS = [6458164021, 6393025468]
 
 # Подключение к PostgreSQL (Supabase)
 DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://postgres.jjrgjqtlwfkejrbhpcsg:v9bGXwQjeK4G9X2M@aws-1-eu-west-1.pooler.supabase.com:5432/postgres")
